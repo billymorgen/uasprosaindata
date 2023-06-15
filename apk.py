722a807c59e90ae49c9bbb52f8cb9d24ad7d2d93
Billy Morgen Simbolon <127178439+billymorgen@users.noreply.github.com>
2023-06-15 18:58:16 +0700

    Add files via upload

---
+++ apk.py
@@ -0,0 +1,10 @@
+import streamlit as st
+def main():
+    st.title("Aplikasi Web Streamlit")
+    st.header("Selamat datang!")
+    st.write("Ini adalah contoh aplikasi web menggunakan Streamlit.")
+    st.image("gambar.png", caption="Gambar contoh")
+    st.write("Terima kasih telah menggunakan aplikasi ini!")
+
+if __name__ == '__main__':
+    main()
